9bf6e6acb767b36151aefa04d329bde513300689
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2022-10-28 11:47:39 +0100

    initialisation

---
+++ database.php
@@ -0,0 +1,5 @@
+<?php
+    
+    //CONNECT TO MYSQL DATABASE USING MYSQLI
+    
+?>
---
+++ scripts.php
@@ -0,0 +1,45 @@
+<?php
+    //INCLUDE DATABASE FILE
+    include('database.php');
+    //SESSSION IS A WAY TO STORE DATA TO BE USED ACROSS MULTIPLE PAGES
+    session_start();
+
+    //ROUTING
+    if(isset($_POST['save']))        saveTask();
+    if(isset($_POST['update']))      updateTask();
+    if(isset($_POST['delete']))      deleteTask();
+    
+
+    function getTasks()
+    {
+        //CODE HERE
+        //SQL SELECT
+        echo "Fetch all tasks";
+    }
+
+
+    function saveTask()
+    {
+        //CODE HERE
+        //SQL INSERT
+        $_SESSION['message'] = "Task has been added successfully !";
+		header('location: index.php');
+    }
+
+    function updateTask()
+    {
+        //CODE HERE
+        //SQL UPDATE
+        $_SESSION['message'] = "Task has been updated successfully !";
+		header('location: index.php');
+    }
+
+    function deleteTask()
+    {
+        //CODE HERE
+        //SQL DELETE
+        $_SESSION['message'] = "Task has been deleted successfully !";
+		header('location: index.php');
+    }
+
+?>
