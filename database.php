3cb9c7e76100279d68cd721b13ede9d1c550a3f9
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2022-10-28 13:56:22 +0100

    Connexion avec dataBase

--- database.php
+++ database.php
@@ -1,5 +1,11 @@
 <?php
-    
+    $serverName = "Localhost";
+    $userName = "YouCode";
+    $password = "";
+    $dataBase ="youcodescrumboard";
     //CONNECT TO MYSQL DATABASE USING MYSQLI
-    
-?>
+    $connexion = mysqli_connect($serverName,$userName,$password,$dataBase);
+    // Check connection
+    if (!$connexion) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
